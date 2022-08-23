8267a65effabea704ced52163c3da4b1655ac9c9
Júnio Moreira <junio.mosilva@gmail.com>
2022-08-23 00:18:41 -0300

    Update index.php
    Correção acessos

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "54.82.69.230";
 $username = "root";
 $password = "Senha123";
-$database = "meubanco";
+$database = "dtbs";
 
 // Criar conexão
 
